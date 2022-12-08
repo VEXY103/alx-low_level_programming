6372eed8173713c413446c0322136fc4b248093d
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-08 15:17:23 -0800

    finally

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *main - entry point
+ *main -entry point
  *Return: Always 0
  */
 int main(void)
 {
-write("and that piece of art is useful" - Dora Korpar, 2015-10-19,\n");
-return(1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
