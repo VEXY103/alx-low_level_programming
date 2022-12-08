838ddab2441b367392676cf9263ade7539a41112
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-08 11:47:37 -0800

    dfg

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,6 +10,6 @@ root@2b65190a739c:/alx-low_level_programming/0x00-hello_world# cat 4-puts.c
 */                                                                                                                              
 int main(void)                                                                                                                  
 {                                                                                                                               
-puts('\'Programming is like building a multilingual puzzle");                                                                   
+puts("\"Programming is like building a multilingual puzzle");
 return (0);                                                                                                                     
-} 
+}
