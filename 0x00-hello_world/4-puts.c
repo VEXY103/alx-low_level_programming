3aa062bab1d2f1cf8623ca3d3dd223e92a90501b
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-08 12:06:52 -0800

    dkfp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>                                                                                                              
+#include <stdio.h>
 /**
-*main - Entry point
-*Return: 0
-*/                                                                                                                              
-int main(void)                                                                                                                  
-{                                                                                                                               
-puts("\"Programming is like building a multilingual puzzle");                                                                   
-return (0);                                                                                                                     
+ *main - entry point
+ *
+ *Return: 0 after printing the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
