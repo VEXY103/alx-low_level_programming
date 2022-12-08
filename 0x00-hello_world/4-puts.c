ff45443d88513d3b587949fec71d90e587393a7a
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-08 11:46:02 -0800

    er

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
 #include <stdio.h>                                                                                                              
 /**                                                                                                                             
-*main - Entry point
-*Return: Always 0 (Success)
-*/
-int main(void)
-{
-puts('\'Programming is like building a multilingual puzzle");
-return (0);
-}
+* main - Entry point                                                                                                            
+* main - Entry point                                                                                                            
+root@2b65190a739c:/alx-low_level_programming/0x00-hello_world# cat 4-puts.c                                                     
+#include <stdio.h>                                                                                                              
+/**                                                                                                                             
+*main - Entry point                                                                                                             
+*Return: Always 0 (Success)                                                                                                     
+*/                                                                                                                              
+int main(void)                                                                                                                  
+{                                                                                                                               
+puts('\'Programming is like building a multilingual puzzle");                                                                   
+return (0);                                                                                                                     
+} 
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* main - Entry point
