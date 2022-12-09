8483124a11e1080502a0bcdbd4abe46582e82c05
VEXY103 <96197591+VEXY103@users.noreply.github.com>
2022-12-09 22:51:06 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,14 +5,15 @@
 */
 int main(void)
 {
-char a;
-for (a = 0 ; a < 10 ; a++)
+int num;
+char letter;
+for (num = 0 ; num < 10 ; num++)
 {
-putchar(a + '0');
+putchar(num % 10 + '0');
 }
 int c;
-for (c = 'a' ; c <= 'f' ; c++)
-{putchar(c);
+for (letter = 'a'; letter <= 'f'; letter++)
+{putchar(letter);
 }
 putchar('\n');
 return (0);
