39492beec3601831514725ff26d31c698a0a7ff2
VEXY103 <96197591+VEXY103@users.noreply.github.com>
2022-12-09 19:09:45 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ else if (last_digit == 0)
 {printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
 else
-{printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+{puts("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }
 return (0);
 }
