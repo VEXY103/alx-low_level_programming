5fa513678d846ad6e55385d75e2ede77fa8c775d
VEXY103 <96197591+VEXY103@users.noreply.github.com>
2022-12-11 09:57:31 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,23 +6,28 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{int a, b, c;
-	for (a = '0' ; a <= '9' ; a++){
-		for (b = '0'; b <= '9'; b++){
-        	for (c = '0'; c <= '9'; c++){
-				if (a < b && b < c){
-				putchar(a);
-				putchar(b);
-                putchar(c);
-				if (a != '7' || (a == '7' && b != '8' && c != '9'))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}	
-        }
-	putchar('\n');
-	return (0);                                                     
-}  
+{
+int a, b, c;
+for (a = '0' ; a <= '9' ; a++)
+{
+for (b = '0'; b <= '9'; b++)
+{
+for (c = '0'; c <= '9'; c++)
+{
+if (a < b && b < c)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+if (a != '7' || (a == '7' && b != '8' && c != '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
