873247db0fd2c9bb9f7068328ff7be6ab21bb66a
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-13 12:14:44 -0800

    prints the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+/**
+ *  *main - start point
+ *   *Return: Always 0 (Success)
+ *    */
+
+int main(void)
+{
+	char l;
+	for(l = 'a'; l <= 'z'; l++)
+	{
+		_putchar(l);
+		_putchar('\n);
+
+	return (0);
+}
