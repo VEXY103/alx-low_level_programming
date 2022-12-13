a04b07b1082d6c023b5e15333541cc962b7c894a
VEXY103 <96197591+VEXY103@users.noreply.github.com>
2022-12-13 20:49:51 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 /**
  *main - start point
  *Return: Always 0 (Success)
  */
-int main(void);{
+
+int main(void){
 _putchar('_');
 _putchar('p');
 _putchar('u');
