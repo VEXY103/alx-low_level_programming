45ee9bd0d6fd35b719f1b7997372bcc496232a94
VEXY103 <96197591+VEXY103@users.noreply.github.com>
2022-12-13 21:28:05 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,9 +6,9 @@
  */
 void print_alphabet(void)
 {
-	char l;
-	
-	for (l = 'a' ; l <= 'z' ; l++)
-		_putchar(l);
-	_putchar('\n');
+char l;
+for (l = 'a' ; l <= 'z' ; l++){
+_putchar(l);
+}
+_putchar('\n');
 }
