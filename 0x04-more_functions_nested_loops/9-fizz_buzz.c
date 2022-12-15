0d53d634b00bdb4f58d8e8ea3f81e8dfe09c181d
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-15 20:28:50 +0100

    Task 9 done

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_square - it prints a square'
+ * size: is the size of the square
+ * Return: void
+ */
+int main(void)
+{
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%n", n);
+		}
+		if (n != 100)
+		{
+			putchar(' ');
+		}
+
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
