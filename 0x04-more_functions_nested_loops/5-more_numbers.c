76cdf6e9aa3b767ef887a25fd2a5c901e8a327f2
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-15 18:20:05 +0100

    Task 5 done

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers : prints 10 times the numbers, from 0 to 14.
+ *
+ * Return: 0 
+ */
+
+void more_numbers(void)
+{
+int c, n;
+n = 0;
+while (n < 10)
+{
+for (c = 0; c <= 14; c++)
+{
+if (c >= 10 && c <= 14)
+{
+_putchar((c / 10) + '0');
+}
+_putchar((c % 10) + '0');
+}
+_putchar('\n');
+n++;
+}
+return(0);
+}
