ba9db446d22a2719994a0358c24df9b4e545fff2
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-19 12:40:02 +0100

    Task 7 done

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - function that prints half of a string.
+ * @str: char to check
+ *
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+int t;
+
+t = 0;
+while (str[t] != '\0')
+{
+t++;
+}
+
+while (t >= t / 2)
+{
+    _putchar(str[t]);
+    t--;
+}
+return (0);
+}
