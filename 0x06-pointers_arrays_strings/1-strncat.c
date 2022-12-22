684d2e79f226d709b20583edbbe3603e285a7aec
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-22 08:12:35 +0100

    Task 1 done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,19 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0, b = 0;
+int a = 0, b = 0;
 
-	while (dest[a] != '\0')
-		a++;
+while (dest[a] != '\0')
+a++;
 
-	while (b < n && src[b] != '\0')
-	{
-		dest[a] = src[b];
-		a++;
-		b++;
-	}
+while (b < n && src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
 
-	dest[a] = '\0';
+dest[a] = '\0';
 
-	return (dest);
+return (dest);
 }
