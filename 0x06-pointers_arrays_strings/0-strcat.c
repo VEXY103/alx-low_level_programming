59fd29151631c46c2b56b612b20fa8b5fe6db0a0
VEXY103 <cherifzaharyasmine@gmail.com>
2022-12-22 08:03:23 +0100

    Fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,8 +12,10 @@ char *_strcat(char *dest, char *src)
 	int a = 0, b = 0;
 
 	while (dest[a] != '\0')
+	{
 		a++;
-
+	}
+		
 	while (src[b] != '\0')
 	{
 		dest[a] = src[b];
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,7 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	int a = 0, b = 0;
 
 	while (dest[a] != '\0')
-		a++;
+    {
+        a++;
+    }
 
 	while (b < n && src[b] != '\0')
 	{
