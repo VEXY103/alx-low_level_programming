5a2c728a7491d0d10b52e636416a576364bba529
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-02 19:56:32 +0100

    Task 3 done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,9 +11,9 @@ unsigned int _strspn(char *s, char *accept)
 {
     int a, b, c;
 
-    for(a = 0; s[a] = "\0"; a++)
+    for(a = 0; s[a] != "\0"; a++)
     {
-        for(b = 0; s[b] = "\0"; b++)
+        for(b = 0; s[b] != "\0"; b++)
         {
             if(s[a] == s[b])
             {
