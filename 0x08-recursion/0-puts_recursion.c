d868b10f46f4bd306e24f06704f630f7dd6d461e
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-04 07:29:07 +0100

    Task 0 done.

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string, followed by a new line.
+ * @s: pointer to the memory area.
+ * Return: s
+ */
+
+void _puts_recursion(char *s)
+{
+    int a;
+
+    if(s[a] == '\0')
+    {
+        _putchar('\n');
+    }
+
+    _puts_recursion(s[a]);
+    a++;
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This is a lesson about recursions and i already fear it.
