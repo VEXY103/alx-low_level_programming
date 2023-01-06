ea210bed0275e139059de1036c3a00c241454eed
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-06 16:32:34 +0100

    Task 0 done.

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,3 +1,27 @@
 #include "main.h"
 
-int _atoi(char *s){}
+/**
+ * _atoi - function that convert a string to an integer.
+ * @s: the string to be comverted.
+ *Return: value.
+ */
+
+int _atoi(char *s)
+{
+	int a = 1;
+	unsigned int n = 0;
+
+	do {
+
+		if (*s == '-')
+			a *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * a);
+}
