8764bfb64f42782a5d550d07bd8be19b0d0eead7
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-10 19:49:03 +0100

    Task 2 done

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,36 +15,38 @@ char *str_concat(char *s1, char *s2)
     char *ptr2;
     int a = 0, b = 0, i, j;
 
-    if (ptr1 == NULL)
+    if (s1 == NULL)
     {
         return (NULL);
     }
 
-    for (i = 0; ptr1[i]; i++)
+    for (i = 0; s1[i]; i++)
     {
         a++;
     }
 
-    ptr1 = malloc(sizeof(char) * a);
+    s1 = malloc(sizeof(char) * a);
 
-    for (j = 0; ptr1[j]; j++)
+    s1[i + 1] = s2[j];
+
+    for (j = 0; s2[j]; j++)
     {
         b++;
     }
 
-    ptr2 = malloc(sizeof(char) * b);
+    s2 = malloc(sizeof(char) * b);
     
     i = 0;
     j = 0;
-    while (*ptr1)
+    while (*s1)
     {
-        _putchar (ptr1[i]);
+        _putchar (s1[i]);
         i++;
     }
     _putchar(' ');
-    while (*ptr2)
+    while (*s2)
     {
-        _putchar (ptr2[j]);
+        _putchar (s2[j]);
         j++;
     }
     return (0);
