c4df4723c17dbdd99a2559af00482514790fb8ce
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-11 06:31:12 +0100

    Task 100 done.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,22 +17,41 @@ int convert(int i)
 	while (i != 0)
 	{
 		if (i % 10 == 9 || i % 10 == 7)
-			i -= 2;
+        {
+            i -= 2;
+        }
+
 		else if (i % 25 == 0)
-			i -= 25;
+        {
+            i -= 25;
+        }
+
 		else if (i % 10 == 0)
-			i -= 10;
+        {
+            i -= 10;
+        }
+
 		else if (i % 5 == 0)
-			i -= 5;
-			else if (i % 2 == 0)
+        {
+            i -= 5;
+        }
+
+		else if (i % 2 == 0)
 		{
 			if (i % 10 == 6)
-				i -= 1;
+            {
+                i -= 1;
+            }
+
 			else
-				i -= 2;
+            {
+                i -= 2;
+            }
 		}
 		else
-			i -= 1;
+        {
+            i -= 1;
+        }
 
 		sum++;
 	}
@@ -63,7 +82,9 @@ int main(int argc, char *argv[])
 	a = atoi(argv[1]);
 
 	if (a < 0)
-		printf("0\n");
+    {
+        printf("0\n");
+    }
 
 	else
 	{
