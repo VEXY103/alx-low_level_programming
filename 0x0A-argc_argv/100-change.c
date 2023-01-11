82afac90e9a7e72e7135647a58bc1f90ad85a6ec
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-11 06:18:55 +0100

    Task 100 done.

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,82 +1,75 @@
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdbool.h>
 
-int main() 
+/**
+ * change - function that'll count the change.
+ * @a: number.
+ * Return: number of coins for the change.
+ */
+
+int change(int a)
 {
-    int n, a = 0, b = 0, c = 0, d = 0, e = 0;
-    int first_rest, second_rest, third_rest, fourth_rest;
-  
-    if (n % 25 == 0)
-    {
-  	    while (a * 25 != n)
-        {
-    	    a++;
-        }
-    }
-    else if (n < 25)
-    {
-        a == 0;
-    }
-    else
-    {
-  	    n % 25 == first_rest;
-    
-        if (first_rest % 10 == 0)
-        {
-    	    while (b * 10 != first_rest)
-            {
-        	    b++;
-            }
-        }
-        else if (first_rest < 10)
-        {
-            b == 0;
-        }
-        else 
-        { 
-  	        first_rest % 10 == second_rest;
-    
-            if (second_rest % 5 == 0)
-            {
-    	        while (c * 5 != second_rest)
-                {
-        	        c++;
-                }
-            }
-            else if (second_rest < 5)
-            {
-                c == 0;
-            }
-            else
-            {
-                second_rest % 5 == third_rest;
-    
-                if (third_rest % 2 == 0)
-                {
-    	            while (d * 2 != third_rest)
-                    {
-        	            d++;
-                    }
-                }
-                else if (third_rest < 2)
-                {
-                    d == 0;
-                }
-                else
-                {
+	int b = 0;
 
-                }
-            }
-    
-        }
-    
-    }
-    int cent;
-    cent = a + b + c + d + e;
+	while (a != 0)
+	{
+		if (a % 10 == 9 || a % 10 == 7)
+			a -= 2;
+		else if (a % 25 == 0)
+			a -= 25;
+		else if (a % 10 == 0)
+			a -= 10;
+		else if (a % 5 == 0)
+			a -= 5;
+			else if (a % 2 == 0)
+		{
+			if (a % 10 == 6)
+				a -= 1;
+			else
+				a -= 2;
+		}
+		else
+			a -= 1;
 
-    return (cent);
-    return (0);
+		b++;
+	}
+
+	return (b);
 }
 
+/**
+ * main - basic function.
+ * @argc: argument count.
+ * @argv: array.
+ * Return: 0 if exactly 1 argument is passed into this program, 1 otherwise.
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, money;
+
+	money = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	a = atoi(argv[1]);
+
+	if (a < 0)
+		printf("0\n");
+
+	else
+	{
+		money = change(a);
+
+		printf("%d\n", money);
+	}
+
+	return (0);
+}
