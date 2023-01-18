01a682129940c2d4152de3c7d8fd328fb84aebd0
VEXY103 <cherifzaharyasmine@gmail.com>
2023-01-18 16:11:00 +0100

    Task 3 done.

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -9,14 +9,6 @@ int op_mod(int a, int b);
 /**
  * op_add - Returns the sum of two numbers.
  * Return: The sum of a and b.
- * op_sub - Returns the difference of two numbers.
- * Return: The difference of a and b.
- * op_mul - Returns the product of two numbers.
- * Return: The product of a and b.
- * op_div - Returns the division of two numbers.
- * Return: The quotient of a and b.
- * op_mod - Returns the remainder of the division of two numbers.
- * Return: The remainder of the division of a by b.
  * @a: The first number.
  * @b: The second number.
  */
@@ -26,20 +18,48 @@ int op_add(int a, int b)
 	return (a + b);
 }
 
+/**
+ * op_sub - Returns the difference of two numbers.
+ * Return: The difference of a and b.
+ * @a: The first number.
+ * @b: The second number.
+ */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
+/**
+ * op_mul - Returns the product of two numbers.
+ * Return: The product of a and b.
+ * @a: The first number.
+ * @b: The second number.
+ */
 
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
 
+/**
+ * op_div - Returns the division of two numbers.
+ * Return: The quotient of a and b.
+ * @a: The first number.
+ * @b: The second number.
+ */
+
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+
+/**
+ * op_mod - Returns the remainder of the division of two numbers.
+ * Return: The remainder of the division of a by b.
+ * @a: The first number.
+ * @b: The second number.
+ */
 
 int op_mod(int a, int b)
 {
